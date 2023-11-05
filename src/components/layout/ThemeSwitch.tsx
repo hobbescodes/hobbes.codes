2bbf734de92cf664cde50aed9f97dcb1953073b4
hobbescodes <jakob.hauble@gmail.com>
2023-11-05 13:17:55 -0600

    feat: add ThemeSwitch component

---
+++ src/components/layout/ThemeSwitch.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Expand } from "@theme-toggles/react";
+import { useTheme } from "next-themes";
+import { useIsClient } from "usehooks-ts";
+
+import "@theme-toggles/react/css/Expand.css";
+
+/**
+ * Switch theme between light mode and dark mode.
+ */
+const ThemeSwitch = () => {
+  const isClient = useIsClient();
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () =>
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+
+  if (!isClient) return null;
+
+  return (
+    <Expand
+      onToggle={toggleTheme}
+      toggled={theme === "light"}
+      className="rounded-md p-3 hover:text-brand-primary-700 dark:hover:text-brand-primary-300"
+    />
+  );
+};
+
+export default ThemeSwitch;
