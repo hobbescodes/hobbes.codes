1a0c9b6d18823404e6795d7201c8383686578787
hobbescodes <jakob.hauble@gmail.com>
2023-11-05 14:10:40 -0600

    feat: add custom Link component

---
+++ src/components/core/Link.tsx
@@ -0,0 +1,40 @@
+import NextLink from "next/link";
+
+import type { LinkProps } from "next/link";
+import type { ReactNode } from "react";
+
+export interface Props extends LinkProps {
+  /** Disable state of link. */
+  isDisabled?: boolean;
+  /** External link. */
+  isExternal?: boolean;
+  className?: string;
+  children?: ReactNode;
+}
+
+/**
+ * Link (application-internal or external).
+ */
+const Link = ({
+  isDisabled,
+  isExternal,
+  className,
+  children,
+  ...rest
+}: Props) =>
+  // forward children if disabled
+  // NB: fragment wrap is intended to prevent return type error
+  isDisabled ? (
+    <>{children}</>
+  ) : (
+    <NextLink
+      target={isExternal ? "_blank" : undefined}
+      rel={isExternal ? "noreferrer noopener" : undefined}
+      className={className}
+      {...rest}
+    >
+      {children}
+    </NextLink>
+  );
+
+export default Link;
