e8a5730baaf3f75fc28bf7a41145984dbb15b5bd
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-11-06 15:07:25 -0600

    feat: add custom Button component

---
+++ src/components/core/Button.tsx
@@ -0,0 +1,64 @@
+import { clsx } from "clsx";
+
+import type { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+
+type Size = "sm" | "md" | "lg" | "xl";
+type Variant = "primary" | "outline" | "unstyled";
+
+export interface Props
+  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  variant?: Variant;
+  size?: Size;
+  children: ReactNode;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+}
+
+/**
+ * Core Button component.
+ */
+const Button = ({
+  children,
+  isLoading,
+  size = "md",
+  variant = "unstyled",
+  isDisabled,
+  className,
+  ...rest
+}: Props) => {
+  const baseClasses = "flex font-medium items-center rounded-md transition-colors duration-300";
+
+  const sizeClasses: Record<Size, string> = {
+    sm: "px-2 py-1 text-xs",
+    md: "px-3 py-2 text-sm",
+    lg: "px-5 py-3 text-base",
+    xl: "px-5 py-3 text-lg",
+  };
+  const variantClasses: Record<Variant, string> = {
+    primary: "gap-2 bg-brand-primary-500 hover:bg-brand-primary-500/80 text-foreground-primary",
+    outline:
+      "gap-2 border border-brand-primary-500 bg-inherit dark:hover:bg-brand-primary-950 hover:bg-brand-primary-50 text-brand-primary-500",
+    unstyled: "",
+  };
+
+  const classes = clsx(
+    baseClasses,
+    sizeClasses[size],
+    variantClasses[variant],
+    isDisabled ? "opacity-40 cursor-not-allowed" : "cursor-pointer",
+    isLoading && "animate-pulse",
+  );
+
+  return (
+    <button
+      className={clsx(classes, className)}
+      disabled={isDisabled || isLoading}
+      aria-disabled={isDisabled}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
