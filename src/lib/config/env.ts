5cc79194df608572c85981c554db68f50175c049
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-11-08 09:01:09 -0600

    chore: set up appropriate redirect URLs

--- src/lib/config/env.ts
+++ src/lib/config/env.ts
@@ -1,2 +1,4 @@
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUBABASE_URL;
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUBABASE_ANON_KEY;
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+export const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
