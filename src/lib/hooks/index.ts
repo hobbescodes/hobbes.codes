ea79175256427426ab30ef4aeccdf2c262689fa2
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-11-13 19:53:18 -0600

    feat(hooks): add custom useDisclosure hook

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -2,4 +2,5 @@ export { default as useBreakpoint } from "./useBreakpoint";
 export { default as useBreakpointValue } from "./useBreakpointValue";
 export { default as useCloseSidebarOnRouteChange } from "./useCloseSidebarOnRouteChange";
 export { default as useCloseSidebarOnWindowResize } from "./useCloseSidebarOnWindowResize";
+export { default as useDisclosure } from "./useDisclosure";
 export { default as useIsTablet } from "./useIsTablet";
